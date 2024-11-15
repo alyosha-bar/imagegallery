4c6fc5be1ba56cf3347fa43fa3de0779efc0af40
Alyosha Baryshnikov <alohahoy@gmail.com>
2024-11-15 18:10:06 +0000

    finish up upload button styling - using SVG

--- src/app/_components/page.tsx
+++ src/app/_components/page.tsx
@@ -1,15 +1,11 @@
-"use client"
-
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { UploadButton } from "~/utils/uploadthing";
+import { SimpleUploadButton } from "./simple-upload-button";
 
 
 function TopNav() {
-
-    const router = useRouter();
-
     return (
       <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold border-b h-24">
         <Link href="/" className="text-3xl p-4"> Gallery </Link>
@@ -18,8 +14,8 @@ function TopNav() {
                 <SignInButton></SignInButton>
             </SignedOut>
             <SignedIn>
-              <div className="flex flex-rol align-center justfify-center">
-                <UploadButton
+              <div className="flex flex-rol items-center justfify-center gap-6">
+                {/* <UploadButton
                   endpoint="imageUploader"
                   onClientUploadComplete={(res) => {
                     // Do something with the response
@@ -30,7 +26,8 @@ function TopNav() {
                     // Do something with the error.
                     alert(`ERROR! ${error.message}`);
                   }}
-                />
+                /> */}
+                <SimpleUploadButton />
                 <UserButton></UserButton>
               </div>
             </SignedIn>
